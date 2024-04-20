ebfb9b52945911436c89b400f4bda8d2b4ee5833
fran <franmach@outlook.com>
2024-04-19 21:35:34 -0300

    ejercicio 6 agregado

---
+++ Practica 2/ej6Practica02/ej6Practica02/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ej6Practica02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese una letra minuscula (desde a hasta f) para saber cual es la siguiente" +
+                "letra del abecedario");
+            string letra = Console.ReadLine();
+
+            switch(letra)
+            {
+                case "a":
+                    Console.WriteLine("La siguiente letra del abecedario es B !!");
+                    break;
+                case "b":
+                    Console.WriteLine("La siguiente letra del abecedario es C !!");
+                    break;
+                case "c":
+                    Console.WriteLine("La siguiente letra del abecedario es D !!");
+                    break;
+                case "d":
+                    Console.WriteLine("La siguiente letra del abecedario es E !!");
+                    break;
+                case "e":
+                    Console.WriteLine("La siguiente letra del abecedario es F !!");
+                    break;
+                case "f":
+                    Console.WriteLine("La siguiente letra del abecedario es G !!");
+                    break;
+
+            }
+        }
+    }
+}
